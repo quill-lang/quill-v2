0c76e8c49f662233e81481d59997f5c2023678e3
thirdsgames <thirdsgames2018@gmail.com>
2022-01-31 17:15:20 +0000

    Complete syntax
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- spec/compile.sh
+++ spec/compile.sh
@@ -1,1 +1,1 @@
-latexmk --shell-escape -pdf --output-directory=build -file-line-error -halt-on-error -interaction=nonstopmode spec.tex
+latexmk --shell-escape -pdflatex=xelatex -pdf --output-directory=build -file-line-error -halt-on-error -interaction=nonstopmode spec.tex
