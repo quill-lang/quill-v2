8bbc3b47c1366e06de5069cc7107b024768b94e0
thirdsgames <thirdsgames2018@gmail.com>
2022-07-10 12:02:07 +0100

    Manually write pre-lexer
    Signed-off-by: thirdsgames <thirdsgames2018@gmail.com>

--- qparse/src/lib.rs
+++ qparse/src/lib.rs
@@ -5,7 +5,6 @@
 
 use std::sync::Arc;
 
-use chumsky::{error::SimpleReason, prelude::*, stream::Stream};
 use fcommon::{Dr, FileReader, Intern, Label, LabelType, Report, ReportKind, Source, Span};
 use upcast::Upcast;
 
@@ -24,96 +23,66 @@ pub enum PreToken {
 }
 
 /// Splits the input stream up into pre-tokens.
-fn pre_lexer() -> impl Parser<char, Vec<(PreToken, Span)>, Error = Simple<char>> {
-    // A pretoken is a sequence of non-whitespace characters.
-    let pretoken = filter(|c: &char| !c.is_whitespace())
-        .map(Some)
-        .chain::<char, Vec<_>, _>(filter(|c: &char| !c.is_whitespace()).repeated())
-        .collect()
-        .map_with_span(|text, span| (PreToken::Lexical { text }, span));
-    let comment =
-        just("//")
-            .then(take_until(text::newline()))
-            .map_with_span(|(_, (text, ())), span| {
-                (
-                    PreToken::Comment {
-                        text: text.into_iter().collect(),
-                    },
-                    span,
-                )
-            });
-    let block_comment =
-        just("/*")
-            .then(take_until(just("*/")))
-            .map_with_span(|(_, (text, _)), span| {
-                (
-                    PreToken::Comment {
-                        text: text.into_iter().collect(),
-                    },
-                    span,
-                )
-            });
-    comment.or(block_comment).or(pretoken).padded().repeated()
-}
-
-pub(crate) fn parse(source: Source, source_contents: &str) -> Dr<Vec<(PreToken, Span)>> {
-    match pre_lexer().parse(source_contents) {
-        Ok(value) => value.into(),
-        Err(errs) => {
-            let mut reports = Vec::new();
-            for e in errs.into_iter() {
-                let msg = format!(
-                    "{}{}, expected {}",
-                    if e.found().is_some() {
-                        "unexpected token"
-                    } else {
-                        "unexpected end of input"
-                    },
-                    if let Some(label) = e.label() {
-                        format!(" while parsing {}", label)
-                    } else {
-                        String::new()
-                    },
-                    if e.expected().len() == 0 {
-                        "something else".to_string()
-                    } else {
-                        e.expected()
-                            .map(|expected| match expected {
-                                Some(expected) => expected.to_string(),
-                                None => "end of input".to_string(),
-                            })
-                            .collect::<Vec<_>>()
-                            .join(", ")
-                    },
-                );
-
-                let report = Report::new(ReportKind::Error, source, e.span().start)
-                    .with_message(msg)
-                    .with_label(Label::new(source, e.span(), LabelType::Error).with_message(
-                        format!(
-                                "unexpected {}",
-                                e.found()
-                                    .map(|c| format!("token {}", c))
-                                    .unwrap_or_else(|| "end of input".to_string())
-                            ),
-                    ));
-
-                let report = match e.reason() {
-                    SimpleReason::Unclosed { span, delimiter } => report.with_label(
-                        Label::new(source, span.clone(), LabelType::Error)
-                            .with_message(format!("unclosed delimiter {}", delimiter)),
-                    ),
-                    SimpleReason::Unexpected => report,
-                    SimpleReason::Custom(msg) => report.with_label(
-                        Label::new(source, e.span(), LabelType::Error).with_message(msg),
-                    ),
-                };
-
-                reports.push(report);
+fn pre_lex(stream: impl Iterator<Item = char>) -> Vec<(PreToken, Span)> {
+    let mut stream = stream.enumerate().peekable();
+    let mut result = Vec::new();
+    loop {
+        // Parse as much whitespace as possible.
+        while let Some((_, c)) = stream.peek() {
+            if c.is_whitespace() {
+                stream.next();
+            } else {
+                break;
             }
-            Dr::fail_many(reports)
         }
+
+        // Parse the next pretoken from the stream.
+        let mut text = String::new();
+        // The start position of this pretoken.
+        let start = if let Some((i, _)) = stream.peek() {
+            *i
+        } else {
+            break;
+        };
+        let mut end = start;
+
+        let pretoken = loop {
+            if let Some((i, c)) = stream.next() {
+                end = i;
+                if c.is_whitespace() {
+                    // This is the end of the pretoken.
+                    break PreToken::Lexical { text };
+                } else {
+                    // This is still part of the pretoken.
+                    text.push(c);
+                    if text.len() == 2 {
+                        // Check whether this is `//` or `/*`.
+                        if text == "//" {
+                            // Parse this pretoken as a line comment.
+                            text = String::new();
+                            for (i, c) in stream.by_ref() {
+                                if c == '\r' || c == '\n' {
+                                    break;
+                                } else {
+                                    text.push(c);
+                                    end = i;
+                                }
+                            }
+                            break PreToken::Comment { text };
+                        }
+                        // TODO: Nested block comments.
+                    }
+                }
+            } else {
+                // This is the end of the file, so the end of the pretoken.
+                break PreToken::Lexical { text };
+            }
+        };
+
+        result.push((pretoken, start..end));
     }
+
+    result
 }
 
 #[salsa::query_group(QuillParserStorage)]
@@ -125,6 +94,6 @@ pub trait QuillParser: FileReader + Upcast<dyn Intern> {
 fn parse_quill(db: &dyn QuillParser, source: Source) -> Dr<Arc<Vec<(PreToken, Span)>>> {
     db.source(source)
         .as_deref()
-        .bind(|source_code| parse(source, source_code))
+        .map(|source_code| pre_lex(source_code.chars()))
         .map(Arc::new)
 }
